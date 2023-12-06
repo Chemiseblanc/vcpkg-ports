b1437d9c7c30d166eb9ab4c684ea217b15280cf2
Matthew Gibson <matt@mgibson.ca>
2023-12-06 11:37:21 -0500

    Updated port of mfem

--- ports/mfem/portfile.cmake
+++ ports/mfem/portfile.cmake
@@ -9,8 +9,54 @@ vcpkg_from_github(
     remove_source_install_dir.patch
 )
 
+vcpkg_check_features(
+    OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        "cuda" MFEM_USE_CUDA
+        "openmp" MFEM_USE_OPENMP
+        "mpi" MFEM_USE_MPI
+        "metis" MFEM_USE_METIS
+        "zlib" MFEM_USE_ZLIB
+        "ginkgo" MFEM_USE_GINKGO
+        "lapack" MFEM_USE_LAPACK
+        "suitesparse" MFEM_USE_SUITESPARSE
+        "sundials" MFEM_USE_SUNDIALS
+        "gnutls" MFEM_USE_GNUTLS
+        "netcdf" MFEM_USE_NETCDF
+        "mpfr" MFEM_USE_MPFR
+        "libunwind" MFEM_USE_LIBUNWIND
+        
+        "gslib" MFEM_USE_GSLIB
+        "ceed" MFEM_USE_CEED
+        "amgx" MFEM_USE_AMGX
+        "conduit" MFEM_USE_CONDUIT
+        "fms" MFEM_USE_FMS
+        "sidre" MFEM_USE_SIDRE # Package actually caleld Axom
+        "moonolith" MFEM_USE_MOONOLITH
+        "hiop" MFEM_USE_HIOP
+        "codipack" MFEM_USE_CODIPACK
+        "occa" MFEM_USE_OCCA
+        "raja" MFEM_USE_RAJA
+        "umpire" MFEM_USE_UMPIRE
+        "caliper" MFEM_USE_CALIPER
+        "algoim" MFEM_USE_ALGOIM
+        "adios2" MFEM_USE_ADIOS2
+        "cpardiso" MFEM_USE_CPARDISO
+        "parelag" MFEM_USE_PARELAG
+        "enzyme" MFEM_USE_ENZYME
+
+        # MPI Libraries
+        "petsc" MFEM_USE_PETSC
+        "superlu" MFEM_USE_SUPERLU
+        "strumpack" MFEM_USE_STRUMPACK
+        "mumps" MFEM_USE_MUMPS
+        "slepc" MFEM_USE_SLEPC
+        "pumi" MFEM_USE_PUMI
+)
+
 vcpkg_cmake_configure(
   SOURCE_PATH "${SOURCE_PATH}"
+  OPTIONS ${FEATURE_OPTIONS}
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
