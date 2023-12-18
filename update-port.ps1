53e5819aa0208bc4507aa4c1e6f0df2a2ca54c55
Matthew Gibson <matt@mgibson.ca>
2023-12-18 17:21:09 -0500

    Improved update-port script

--- update-port.ps1
+++ update-port.ps1
@@ -1,3 +1,9 @@
 $port = $args[0]
+
 & vcpkg format-manifest ".\ports\$port\vcpkg.json"
+& git add ".\ports\$port"
+& git commit -m "Updated port of $port"
+
 & vcpkg --x-builtin-ports-root=.\ports --x-builtin-registry-versions-dir=.\versions x-add-version  $port --verbose
+& git add ".\versions"
+& git commit -m "Updated baseline of $port"
