b5555c8435e7908c7d40a3e2e64af7f6ce5496d8
Matthew Gibson <matt@mgibson.ca>
2024-01-13 16:48:12 -0500

    Updated port of dataflow

--- ports/dataflow/portfile.cmake
+++ ports/dataflow/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
         OUT_SOURCE_PATH SOURCE_PATH
         REPO chemiseblanc/dataflow
         REF 0d5b81cf488b7b489e7bbc6f960745551a0c8352
-        SHA512 0
+        SHA512 180a8bec081c20da023f382c7e2f24bc18482ad1e70914de2036a1cd9f1c9bea9a2f6eaac9d6b68f7d6e4f1b73b7e7d80ce9c351987609f0e8751e48f15b2ee9
         HEAD_REF main
 )
 
