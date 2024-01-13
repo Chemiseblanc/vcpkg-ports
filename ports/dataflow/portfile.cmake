64bfdf4da784226bceba45b03bee29519d4d52de
Matthew Gibson <matt@mgibson.ca>
2024-01-13 17:19:46 -0500

    Updated port of dataflow

--- ports/dataflow/portfile.cmake
+++ ports/dataflow/portfile.cmake
@@ -8,6 +8,8 @@ vcpkg_from_github(
 
 vcpkg_cmake_configure(
         SOURCE_PATH "${SOURCE_PATH}"
+        OPTIONS
+            DATAFLOW_BUILD_EXAMPLES=OFF
 )
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup()
